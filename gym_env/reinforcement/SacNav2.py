17f601200108240d3b31acb2080ba8e1d27eae53
ParakramV <bravocharlieawesome@gmail.com>
2024-09-18 14:20:34 +0800

    Sac trainer updated SacNav2

--- gym_env/reinforcement/SacNav2.py
+++ gym_env/reinforcement/SacNav2.py
@@ -8,19 +8,56 @@ from stable_baselines3.common.results_plotter import ts2xy, load_results
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 import os
 
+def plot_results(log_folder, title="Learning Curve"):
+    """
+    Plot the results.
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), "timesteps")
+    
+    # print(x, y)
+    fig = plt.figure(title)
+    plt.plot(x, y)
+    plt.xlabel("Number of Timesteps")
+    plt.ylabel("Rewards")
+    plt.title(title)
+    plt.savefig('./graphs/SAC_results.png')
+    print("Graph Saved")
+    plt.show()
+
+
 path = os.getcwd()
 parent = os.path.dirname(path)
 log_dir = "/tmp/gym/"
 path = os.path.join(parent, log_dir)
 # print("path", path)
 os.makedirs(path, exist_ok=True)
+
+lr = 0.003
+
 env = CustomGymnasiumEnvNav2()
+
 env = Monitor(env, log_dir)
-model = SAC("MultiInputPolicy", env, verbose=1)
+
+model = SAC("MultiInputPolicy", env, learning_rate=lr, verbose=1)
 #learn the model
 model.learn(total_timesteps=200000, log_interval=10)
 #save learnt model
-model.save("./models/SAC_trained_nav")
+model.save(f"./models/SAC_trained_nav2_{lr}_3000")
+
+#get training results and save to csv
+df = load_results(log_dir)
+# print(f"There are {len(df)} results")
+df.to_csv(f"./results/SAC_training_results_18_09_lr_{lr}_epLen_{3000}.csv", index=False)
+print("Training Results Written")
+
+#plot training results
+results_plotter.plot_results([log_dir], 1e5, results_plotter.X_TIMESTEPS, "SAC Results")
+plot_results(log_dir)
+
+
 
 
 
